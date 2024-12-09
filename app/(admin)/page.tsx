24600f51869e114e6f9aa59bce053a92dec14379
danil2w3 <danilstepanov1331@gmail.com>
2024-12-10 03:43:30 +0600

    first com

---
+++ app/(admin)/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+
+      <main className="">
+        <h1>Lest build an Assistly AI Agent App</h1>
+      </main>
+  );
+}
---
+++ graphql/mutations/mutations.ts
@@ -0,0 +1,47 @@
+import { gql } from "@apollo/client";
+
+export const CREATE_CHATBOT = gql`
+  mutation CreateChatbot($clerk_user_id: String!, $name: String!) {
+    insertChatbots(clerk_user_id: $clerk_user_id, name: $name, created_at: "2024-12-10T00:00:00Z") {
+      id
+      name
+    }
+  }
+`;
+
+export const REMOVE_CHARACTERISTIC = gql`
+  mutation RemoveCharacteristic($characteristicId: Int!) {
+    deleteChatbot_characteristics( id: $characteristicId) {
+      id
+    } 
+  }
+`;
+
+export const DELETE_CHATBOT = gql`
+  mutation DeleteChatbot($id: Int!) {
+    deleteChatbots(id: $id) { 
+      id
+    }
+  }
+`;
+
+export const ADD_CHARACTERISTIC = gql`
+  mutation AddCharacteristic($chatbotId: Int!, $content: String!) {
+    insertChatbot_characteristics(chatbot_id: $chatbotId, content: 
+    $content) {
+      id
+      content
+      created_at 
+    }
+  }
+`;
+
+export const UPDATE_CHATBOT = gql`
+mutation UpdateChatbot($id: Int!, $name: String!) {
+  updateChatbots(id: $id, name: $name) {
+    id
+    name
+    created_at
+   }
+  }
+`;
